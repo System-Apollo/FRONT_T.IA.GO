f68a50c77dc54988bab7856f3ab1a84b8b853b7d
leticia nobre <leticiavidal2910@gmail.com>
2024-11-06 18:47:26 -0300

    ✨ feat: added background img and overlay

--- src/pages/sobre/secondSection/index.tsx
+++ src/pages/sobre/secondSection/index.tsx
@@ -1,10 +1,20 @@
-
-
 export default function SecondSection () {
     return (
-        <section className="relative overflow-hidden py-28 px-4 bg-gray-900 md:px-8 w-full">
+        <section 
+            className="relative overflow-hidden py-28 px-4 bg-gray-900 md:px-8 w-full"
+            style={{
+                backgroundImage: "url('/backgroundimg.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            {/* Overlay azul */}
+            <div className="absolute inset-0 bg-blue-500 opacity-50"></div>
+
             <div className="w-full h-full rounded-full bg-gradient-to-r from-[#58AEF1] to-pink-500 absolute -top-12 -right-14 blur-2xl opacity-10"></div>
-            <div className="max-w-xl mx-auto text-center relative">
+            
+            <div className="max-w-xl mx-auto text-center relative z-10">
                 <div className="py-4">
                     <h3 className="text-3xl text-gray-200 font-semibold md:text-4xl">
                         Get Unlimited Access To All Templates
