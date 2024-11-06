7182bec51bba9a0eb3fc28fafe3c94a69be0c5d5
leticia nobre <leticiavidal2910@gmail.com>
2024-11-06 19:48:31 -0300

    ✨ feat: added secondsection to solucoes index

--- src/pages/solucoes/index.tsx
+++ src/pages/solucoes/index.tsx
@@ -1,10 +1,12 @@
 import FirstSection from "./firstSection";
+import SecondSection from "./secondSection";
 
 
 export default function Solucoes() {
   return (
     <>
       <FirstSection/>
+      <SecondSection/>
     </>
   );
 }
