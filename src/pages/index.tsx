cc49a76377526330cb3b2a1874ef4efa20ab081a
leticia nobre <leticiavidal2910@gmail.com>
2024-11-06 12:22:26 -0300

    ✨ feat: added Initial to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import { Inter } from 'next/font/google';
+import Initial from './Initial'
 
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <main className={`flex min-h-screen flex-col items-center justify-between font-sans ${inter.className}`}>
+      <Initial/>
+    </main>
   );
 }
