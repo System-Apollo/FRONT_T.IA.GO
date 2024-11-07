b8d4d31e68e5c610909375ec575e58b91813e822
leticia nobre <leticiavidal2910@gmail.com>
2024-11-07 14:55:17 -0300

    ♻️ refat: updated text in cards

--- src/pages/planos/index.tsx
+++ src/pages/planos/index.tsx
@@ -2,49 +2,47 @@ export default function Planos () {
 
     const plans = [
         {
-            name: "Basic plan",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: 12,
+            name: "Plano Trainee",
+            desc: "Ideal para conhecer o TIAGO. Gratuito por apenas 7 dias.",
+            price: 0,
             isMostPop: false,
             features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
+                "1 usuário",
+                "2 gráficos",
+                "Implantação automática",
+                "Limitado a 20 processos",
+                "Ilimitado por 7 dias",
 
             ],
         },
         {
-            name: "Startup",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: 35,
+            name: "Plano Assistente",
+            desc: "Ideal para clientes com até 500 processos ativos.",
+            price: "2.500,00",
             isMostPop: true,
             features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
+                "1 usuário",
+                "2 gráficos",
+                "Implantação em até 30 dias",
+                "Limitado a 500 processos",
+                "Até 200 perguntas (tokens)",
+                "Suporte pelo Whatsapp"
+
             ],
         },
         {
-            name: "Enterprise",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            price: 60,
+            name: "Plano Analista",
+            desc: "Ideal para clientes com até 2000 processos ativos.",
+            price: "5.000,00",
             isMostPop: false,
             features: [
-                "Curabitur faucibus",
-                "massa ut pretium maximus",
-                "Sed posuere nisi",
-                "Pellentesque eu nibh et neque",
-                "Suspendisse a leo",
-                "Praesent quis venenatis ipsum",
-                "Duis non diam vel tortor",
+                "1 usuário",
+                "7 gráficos",
+                "Implantação em até 30 dias",
+                "Limitado a 200 processos",
+                "Até 400 perguntas (tokens)",
+                "Suporte pelo Whatsapp",
+                "Acesso via Telegram"
             ],
         },
     ];
@@ -53,45 +51,45 @@ export default function Planos () {
         <section className='py-14 bg-gradient w-full'>
             <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
                 <div className='relative max-w-xl mx-auto sm:text-center'>
-                    <h3 className='text-gray-800 text-3xl font-semibold sm:text-4xl'>
-                        Pricing for all sizes
+                    <h3 className='text-indigo-50 text-3xl font-semibold sm:text-4xl'>
+                        Nossos planos
                     </h3>
-                    <div className='mt-3 max-w-xl'>
+                    <div className='mt-3 max-w-xl text-gray-400'>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur consequat nunc.
+                            Escolha o plano ideal e o seu investimento em Tecnologia Jurídica
                         </p>
                     </div>
                 </div>
                 <div className='mt-16 justify-center gap-6 sm:grid sm:grid-cols-2 sm:space-y-0 lg:grid-cols-3'>
                     {
                         plans.map((item, idx) => (
-                            <div key={idx} className={`relative flex-1 flex items-stretch flex-col rounded-xl border-2 border-gray-700 bg-dark mt-6 sm:mt-0 ${item.isMostPop ? "mt-10" : ""}`}>
+                            <div key={idx} className={`relative flex-1 flex items-stretch flex-col rounded-xl border border-gray-700 bg-dark mt-6 sm:mt-0 ${item.isMostPop ? "mt-10" : ""}`}>
                                 {
                                     item.isMostPop ? (
-                                        <span className="w-32 absolute -top-5 left-0 right-0 mx-auto px-3 py-2 rounded-full border shadow-md bg-white text-center text-gray-700 text-sm font-semibold">Most popular</span>
+                                        <span className="w-32 absolute -top-5 left-0 right-0 mx-auto px-3 py-2 rounded-full border border-gray-700 shadow-md bg-gray-700 text-center text-indigo-50 text-sm font-semibold">Mais popular</span>
                                     ) : ""
                                 }
                                 <div className="p-8 space-y-4 border-b border-gray-700">
                                     <span className='text-gradient font-medium'>
                                         {item.name}
                                     </span>
-                                    <div className='text-gray-800 text-3xl font-semibold'>
-                                        ${item.price} <span className="text-xl text-gray-600 font-normal">/mo</span>
+                                    <div className='text-indigo-100 text-3xl font-semibold'>
+                                        R$ {item.price} <span className="text-xl text-gray-600 font-normal">/mês</span>
                                     </div>
-                                    <p>
+                                    <p className='text-gray-400'>
                                         {item.desc}
                                     </p>
-                                    <button className='px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700'>
-                                        Get Started
+                                    <button className='px-3 py-3 rounded-lg w-full font-semibold text-sm duration-150 text-gray-800 bg-indigo-100 hover:text-gray-600 active:bg-indigo-700'>
+                                        Começar
                                     </button>
                                 </div>
                                 <ul className='p-8 space-y-3'>
-                                    <li className="pb-2 text-gray-800 font-medium">
-                                        <p>Features</p>
+                                    <li className="pb-2 text-indigo-100 font-medium">
+                                        <p>Acompanha</p>
                                     </li>
                                     {
                                         item.features.map((featureItem, idx) => (
-                                            <li key={idx} className='flex items-center gap-5'>
+                                            <li key={idx} className='flex items-center text-gray-400 gap-5'>
                                                 <svg
                                                     xmlns='http://www.w3.org/2000/svg'
                                                     className='h-5 w-5 text-green-500'
