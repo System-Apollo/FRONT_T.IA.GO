0bfba471de87318ab5f9be277265238086c70dc9
leticia nobre <leticiavidal2910@gmail.com>
2024-11-07 13:00:05 -0300

    ⚰️ delete: removed dead code

--- src/pages/footer/index.tsx
+++ src/pages/footer/index.tsx
@@ -1,13 +1,6 @@
 import { FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 export default function Footer() {
-    const footerNavs = [
-        { href: 'javascript:void()', name: 'About' },
-        { href: 'javascript:void()', name: 'Blog' },
-        { href: 'javascript:void()', name: 'Team' },
-        { href: 'javascript:void()', name: 'Careers' },
-        { href: 'javascript:void()', name: 'Support' }
-    ];
 
     return (
         <footer className="text-gray-500 bg-gradient px-4 py-5 w-full mx-auto md:px-8">
@@ -17,14 +10,7 @@ export default function Footer() {
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
             </div>
-            <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
-                {footerNavs.map((item, idx) => (
-                    <li className="hover:text-gray-800" key={idx}>
-                        <a href={item.href}>{item.name}</a>
-                    </li>
-                ))}
-            </ul>
-            <div className="mt-8 items-center justify-between sm:flex">
+            <div className="mt-8 py-6 border-t border-slate-700 items-center justify-between sm:flex">
                 <div className="mt-4 sm:mt-0">
                     &copy; 2022 Float UI All rights reserved.
                 </div>
