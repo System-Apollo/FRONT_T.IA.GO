02ac42ca939c59a7905470c930770929fc34bd82
leticia nobre <leticiavidal2910@gmail.com>
2024-11-07 13:52:33 -0300

    ✨ feat: added to home page and added image

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -1,12 +1,14 @@
 import { Inter } from 'next/font/google';
 import FirstSection from './firstSection';
 import SecondSection from './secondSection';
+import Tiago from './tiago';
 
 export default function Sobre() {
   return (
     <>
       <FirstSection/>
       <SecondSection/>
+      <Tiago/>
     </>
   );
 }
