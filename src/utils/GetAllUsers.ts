800d1626e52588044a90ef672b469ff6f2d47a16
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2024-12-02 10:15:03 -0300

    feat: created userGet util

--- src/utils/GetAllUsers.ts
+++ src/utils/GetAllUsers.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosResponse } from 'axios';
 
 const GetAllUsers = async (): Promise<AxiosResponse<any>> => {
-  const API = process.env.NEXT_PUBLIC_API_HOMOLOG_USER;
+  const API = process.env.NEXT_PUBLIC_API_HOMOLOG;
   const token = localStorage.getItem("token");
 
   try {
-    const response: AxiosResponse<any> = await axios.get(`${API}/users/view`, {
+    const response: AxiosResponse<any> = await axios.get(`${API}/user/all`, {
       headers: {
         Authorization: `Bearer ${token}`,
         "ngrok-skip-browser-warning": "69420",
